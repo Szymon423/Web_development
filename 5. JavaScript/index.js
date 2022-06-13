82e04480a7e8b4ec6059bcd345f74e73388187d2
Szymon <wachim17@gmail.com>
2022-06-14 00:15:42 +0200

    całkiem spotko ten js

--- 5. JavaScript/index.js
+++ 5. JavaScript/index.js
@@ -1,8 +1,13 @@
-var dupa = "dupa";
-alert(dupa);
-
 var name = prompt("czym się kończą plecy?");
 alert("plecy kończą się " + name);
 
-dupsko = "cucki";
+dupsko = "cycki";
 alert("hello " + "world " + dupsko);
+
+var ile = dupsko.length;
+
+
+var text = "dupa cycki jebać cweli i pedałów"
+
+var dupa = text.slice(0, 4);
+alert(dupa);
