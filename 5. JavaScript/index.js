8e46ae7ffb5e60544c0f7f6b146774fd3ff3928f
Szymon_IASE <wachim17@gmail.com>
2022-06-17 13:54:03 +0200

    fun

--- 5. JavaScript/index.js
+++ 5. JavaScript/index.js
@@ -7,7 +7,7 @@ alert("hello " + "world " + dupsko);
 var ile = dupsko.length;
 
 
-var text = "przykładowy tekst tsasasasaest"
+var text = "przykładowy tekst test"
 
 var dupa = text.slice(0, 4);
 alert(dupa);
@@ -23,3 +23,14 @@ var first = imie.slice(0, 1);
 var last = imie.slice(1, imie.length);
 
 alert("siema gżdylu, " + first.toUpperCase() + last.toLowerCase());
+
+function bmi() {
+    var weight = prompt("podaj wagę grubasie");
+    var height = prompt("ile masz metrów karle?");
+    return weight / height ** 2
+}
+
+
+bmi()
+
+
