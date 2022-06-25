c3bd0f622e4c2e7038c8827f4990994d2acfb5ba
Szymon <wachim17@gmail.com>
2022-06-25 14:18:22 +0200

    fun fun

---
+++ 6. DOM/index.js
@@ -0,0 +1,3 @@
+document.querySelectorAll(".list")[2].style.color = "red";
+document.querySelectorAll(".list")[2].style.fontSize = "10rem";
+document.querySelector("button").style.backgroundColor = "yellow";
