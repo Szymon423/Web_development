4cd5e3eb4c884cfc389dc3a3cece3e061175cddd
Szymon_IASE <wachim17@gmail.com>
2022-06-28 13:21:20 +0200

    jQuerry

--- 9. jQuerry/index.js
+++ 9. jQuerry/index.js
@@ -1,1 +1,18 @@
-$("h1").css("color", "white");
+$("button").html("<em>white</em>");
+
+$("h1").addClass("big-title");
+
+// alert($("h1").hasClass("big-title"));
+
+// alert($("h1").attr("class"));
+
+// obsługa kliknięcia 
+$("button").click(function () {
+    $("h1").slideUp().slideDown();
+    
+});
+
+$("h1").on("mouseover", function (event) {
+    $("h1").text("hello");
+    
+});
