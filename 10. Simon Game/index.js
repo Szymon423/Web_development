263e6c7a0bcb1289d8213f248615346a4c440300
Szymon_IASE <wachim17@gmail.com>
2022-06-28 14:32:33 +0200

    update

--- 10. Simon Game/index.js
+++ 10. Simon Game/index.js
@@ -1,26 +1,75 @@
-//  pattern of buttons
- var gamePattern = [];
+// user level
+var lvl = 0;
 
-//  avaliable buttons
- var buttonColours = ["red", "blue", "green", "yellow"];
+// user clicks counter
+var clicks = 0;
+
+// user pattern
+var userClickedPattern = [];
+
+// pattern of buttons
+var gamePattern = [];
+
+ // avaliable buttons
+var buttonColours = ["red", "blue", "green", "yellow"];
  
-//  randomly choosing number from 0 - 3
- var randomChosenColour = buttonColours[randomNumber()]
+// randomly choosing number from 0 - 3
+var randomChosenColour = buttonColours[nextSequence()]
 
-//  aktualizacja patternu dla gry
- gamePattern.push(randomChosenColour);
+// aktualizacja patternu dla gry
+gamePattern.push(randomChosenColour);
  
-// mignięcie
- $("." + randomChosenColour).fadeOut(70).fadeIn(70);
-
 // odtworzenie dźwięku
-var name_ = "sounds/" + randomChosenColour + ".mp3";
-alert(name_);
-var audio = new Audio(name_);
-audio.play();
+playSound(randomChosenColour);
 
+// mignięcie
+$("." + randomChosenColour).fadeOut(70).fadeIn(70);
 
-//  function for generating random numbers
- function randomNumber() {
-    return Math.floor(Math.random() * 4);
+$(".btn").click(function(event) {
+   userChosenColour = this.id;
+
+   //  play the coresponging sound
+   playSound(userChosenColour);
+   userClickedPattern.push(userChosenColour);
+
+   animatePress(userChosenColour);
+   // increasing clicking counter
+   clisks++;
+ });
+
+ $("body").keypress(function() {
+  $("h1").text("Level " + lvl);
+ });
+
+ if (clicks === lvl) {
+  checkAnswer();
  }
+
+
+// checking answer function
+function checkAnswer() {
+  var it = (userClickedPattern === gamePattern);
+  alert(it);
+  return it;
+}
+
+
+// animation of the press
+function animatePress(btn) {
+  $("." + btn).addClass("pressed");
+
+  setTimeout(function() {
+      $("." + btn).removeClass("pressed");
+  }, 100);
+}
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  lvl++;
+ }
+
+function playSound(sound) {
+   sound = new Audio("sounds/" + sound + ".mp3");
+   sound.play();
+}
+
